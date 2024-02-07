b65586f151d161d94b288157d2ae9b24d74d8c93
Kuldeep tak <Kuldeeptak2211@gmail.com>
2024-02-07 12:02:08 +0530

    added: tailwind css ,bootstrap, Todo component

---
+++ src/components/Todo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const Todo = () => {
+  return (
+    <Container className="my-5" fluid >
+      <Row>
+        <Col>
+            My TOdos
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Todo;
